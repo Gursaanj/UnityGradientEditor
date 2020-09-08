0465c2c86d8ad8284a4a3d776b38817d7202d606
Gursaanj <gursaanjresides@gmail.com>
2020-09-08 00:24:06 -0700

    Fully working Gradient Editor

--- GradientEditor/Assets/Editor/GradientEditor.cs
+++ GradientEditor/Assets/Editor/GradientEditor.cs
@@ -1,14 +1,26 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class GradientEditor : EditorWindow
 {
+    private const int WindowWidth = 400;
+    private const int MinWindowWidth = 200;
+    private const int MaxWindowWidth = 1920;
+    private const int WindowHeight = 150;
     private const int BorderSize = 10;
     private const int GradientRectHeight = 25;
     private const int KeyWidth = 10;
     private const int KeyHeight = 20;
+    private const int SelectedKetOffset = 2;
     private CustomGradient _gradient;
+    private Rect _gradientPreviewRect;
+    private Rect _keyRect;
+    private Rect[] _keyRects;
+    private int _selectedKeyIndex = 0;
+    private bool _isMouseDownOverKey = false;
+    private bool _needsRepaint = false;
 
     public void SetGradient(CustomGradient gradient)
     {
@@ -18,27 +30,133 @@ public class GradientEditor : EditorWindow
     private void OnEnable()
     {
         titleContent.text = "Gradient Editor";
+        position.Set(position.x, position.y,WindowWidth, WindowHeight);
+        minSize = new Vector2(MinWindowWidth, WindowHeight);
+        maxSize = new Vector2(MaxWindowWidth, WindowHeight);
+    }
+    
+    // Mark the Scene as dirty to save the changes when reopening
+    private void OnDisable()
+    {
+        UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
     }
 
     private void OnGUI()
     {
-        Event guiEvent = Event.current;
-        Rect gradientPreviewRect = new Rect(BorderSize, BorderSize, position.width - BorderSize*2, GradientRectHeight);
-        GUI.DrawTexture(gradientPreviewRect, _gradient.GetTexture((int) gradientPreviewRect.width));
-        
-        for (int i = 0; i < _gradient.NumberOfKeys(); i++)
-        { 
-            CustomGradient.ColourKey key = _gradient.GetKey(i);
-            Rect keyRect = new Rect(gradientPreviewRect.x + gradientPreviewRect.width * key.Time - KeyWidth/2f, gradientPreviewRect.yMax + BorderSize, KeyWidth, KeyHeight);
-            EditorGUI.DrawRect(keyRect, key.Colour);
-        }
+        Draw();
+        HandleInput();
 
-        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0)
+        if (_needsRepaint)
         {
-            Color randomColour = new Color(Random.value, Random.value, Random.value);
-            float keyTime = Mathf.InverseLerp(gradientPreviewRect.x, gradientPreviewRect.xMax, guiEvent.mousePosition.x);
-            _gradient.AddKey(randomColour, keyTime);
+            _needsRepaint = false;
             Repaint();
         }
+    }
+
+    private void Draw()
+    {
+        _gradientPreviewRect = new Rect(BorderSize, BorderSize, position.width - BorderSize*2, GradientRectHeight);
+        GUI.DrawTexture(_gradientPreviewRect, _gradient.GetTexture((int) _gradientPreviewRect.width));
+        int numberOfKeys = _gradient.NumberOfKeys();
+
+        if (numberOfKeys != -1)
+        {
+            _keyRects = new Rect[numberOfKeys];
+        
+            for (int i = 0; i <numberOfKeys; i++)
+            { 
+                CustomGradient.ColourKey key = _gradient.GetKey(i);
+                _keyRect = new Rect(_gradientPreviewRect.x + _gradientPreviewRect.width * key.Time - KeyWidth/2f, _gradientPreviewRect.yMax + BorderSize, KeyWidth, KeyHeight);
+                
+                //Give a black background to the selected key
+                if (i == _selectedKeyIndex)
+                {
+                    EditorGUI.DrawRect( new Rect(_keyRect.x - SelectedKetOffset, _keyRect.y - SelectedKetOffset, _keyRect.width + (SelectedKetOffset * 2),
+                        _keyRect.height + (SelectedKetOffset * 2)), Color.black);
+                }
+
+                EditorGUI.DrawRect(_keyRect, key.Colour);
+                _keyRects[i] = _keyRect;
+            }
+            
+            Rect settingsRect = new Rect(BorderSize, _keyRect.yMax + BorderSize, position.width -BorderSize*2, position.height);
+
+            using (new GUILayout.AreaScope(settingsRect))
+            {
+                using (EditorGUI.ChangeCheckScope changeCheck = new EditorGUI.ChangeCheckScope())
+                {
+                    Color newColour = EditorGUILayout.ColorField(_gradient.GetKey(_selectedKeyIndex).Colour);
+
+                    if (changeCheck.changed)
+                    {
+                        _gradient.UpdateKeyColour(_selectedKeyIndex, newColour);
+                    }
+                }
+
+                _gradient.blendMode = (CustomGradient.BlendMode)EditorGUILayout.EnumPopup("Blend Mode", _gradient.blendMode);
+                _gradient.shouldRandomizeColour = EditorGUILayout.Toggle("Randomize Colour", _gradient.shouldRandomizeColour);
+            }
+        }
+    }
+
+    private void HandleInput()
+    {
+        Event guiEvent = Event.current;
+       if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0)
+        {
+            for (int i = 0; i < _keyRects.Length; i++)
+            {
+                if (_keyRects[i].Contains(guiEvent.mousePosition))
+                {
+                    _isMouseDownOverKey = true;
+                    _selectedKeyIndex = i;
+                    _needsRepaint = true;
+                    break;
+                }
+            }
+
+
+            if (!_isMouseDownOverKey)
+            {
+                float keyTime = Mathf.InverseLerp(_gradientPreviewRect.x, _gradientPreviewRect.xMax, guiEvent.mousePosition.x);
+                Color interpolatedColour = _gradient.Evaluate(keyTime);
+                Color randomColour = new Color(Random.value, Random.value, Random.value);
+                _selectedKeyIndex = _gradient.AddKey(_gradient.shouldRandomizeColour ? randomColour : interpolatedColour, keyTime);
+                _isMouseDownOverKey = true;
+                _needsRepaint = true;
+            }
+        }
+
+        if (guiEvent.type == EventType.MouseUp && guiEvent.button == 0)
+        {
+            _isMouseDownOverKey = false;
+        }
+        
+        //Handles dragging of ColourKey
+        if (_isMouseDownOverKey && guiEvent.type == EventType.MouseDrag && guiEvent.button == 0)
+        {
+            float keyTime = Mathf.InverseLerp(_gradientPreviewRect.x, _gradientPreviewRect.xMax, guiEvent.mousePosition.x);
+            int updatedKeyIndex = _gradient.UpdateKeyTime(_selectedKeyIndex, keyTime);
+
+            if (updatedKeyIndex != -1)
+            {
+                _selectedKeyIndex = updatedKeyIndex;
+                _needsRepaint = true;
+            }
+        }
+        
+        //Handles Deleting of ColourKey
+        if (guiEvent.type == EventType.KeyDown && (guiEvent.keyCode == KeyCode.Backspace || guiEvent.keyCode == KeyCode.Delete))
+        {
+            _gradient.RemoveKey(_selectedKeyIndex);
+            
+            // handles if selectedkey now goes out of range
+            if (_selectedKeyIndex >= _gradient.NumberOfKeys())
+            {
+                _selectedKeyIndex--;
+            }
+
+            _needsRepaint = true;
+        } 
     }
 }
--- GradientEditor/Assets/Scripts/CustomGradient.cs
+++ GradientEditor/Assets/Scripts/CustomGradient.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [Serializable]
 public class CustomGradient
 {
+    public BlendMode blendMode;
+    public bool shouldRandomizeColour;
     [SerializeField] private List<ColourKey> keys = new List<ColourKey>();
     
     [Serializable]
@@ -23,6 +25,18 @@ public class CustomGradient
         public float Time => time;
     }
     
+    public enum BlendMode
+    {
+        Linear,
+        Discrete
+    }
+
+    public CustomGradient()
+    {
+        AddKey(Color.white, 0);
+        AddKey(Color.black, 1);
+    }
+
     public Color Evaluate(float time)
     {
         if (keys == null || keys.Count == 0)
@@ -33,35 +47,60 @@ public class CustomGradient
         ColourKey keyLeft = keys[0];
         ColourKey keyRight = keys[keys.Count - 1];
 
-        for (int i = 0; i < keys.Count-1; i++)
+        for (int i = 0; i < keys.Count; i++)
         {
             ColourKey currentKey = keys[i];
-            ColourKey nextKey = keys[i + 1];
             
             //if the designated time is in between two consecutive keys Create new KeysLeft and KeysRight 
-            if (currentKey.Time <= time && nextKey.Time >= time)
+            if (currentKey.Time <= time)
             {
                 keyLeft = currentKey;
-                keyRight = nextKey;
+            }
+
+            if (currentKey.Time >= time)
+            {
+                keyRight = currentKey;
                 break;
             }
         }
         
-        //return blend between ketLeft and keyRight
-        
-        // Will convert time to a float (0,1) in reference to keyLeft(0) and keyRight(1)
-        float blendTime = Mathf.InverseLerp(keyLeft.Time, keyRight.Time, time);
-        return Color.Lerp(keyLeft.Colour, keyRight.Colour, blendTime);
-    }
+        //return blend between ketLeft and keyRight depending on blendmode
 
-    public void AddKey(Color colour, float time)
+        switch (blendMode)
+        {
+            case BlendMode.Linear:
+            {
+                // Will convert time to a float (0,1) in reference to keyLeft(0) and keyRight(1)
+                float blendTime = Mathf.InverseLerp(keyLeft.Time, keyRight.Time, time);
+                return Color.Lerp(keyLeft.Colour, keyRight.Colour, blendTime);
+            }
+            case BlendMode.Discrete:
+            {
+                return keyRight.Colour;
+            }
+            default:
+            {
+                // Will convert time to a float (0,1) in reference to keyLeft(0) and keyRight(1)
+                float blendTime = Mathf.InverseLerp(keyLeft.Time, keyRight.Time, time);
+                return Color.Lerp(keyLeft.Colour, keyRight.Colour, blendTime);
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Adds Colour Key to List and returns index placed
+    /// </summary>
+    /// <param name="colour"></param>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    public int AddKey(Color colour, float time)
     {
         ColourKey newKey = new ColourKey(colour, time);
 
         if (keys == null)
         {
             Debug.LogError("The list Of ColourKeys is empty");
-            return;
+            return -1;
         }
         
         for (int i = 0; i < keys.Count; i++)
@@ -69,12 +108,61 @@ public class CustomGradient
             if (newKey.Time < keys[i].Time) 
             {
                 keys.Insert(i, newKey);
-                return;
+                return i;
             }
         }
         
         keys.Add(newKey);
+        return keys.Count - 1;
     }
+    
+    // We always want at least one key in our gradient
+    public void RemoveKey(int index)
+    {
+        if (keys == null || index >= keys.Count)
+        {
+            Debug.LogError("Either keys is null or the index does not exist in the list");
+            return;
+        }
+
+        if (keys.Count < 2)
+        {
+            Debug.LogWarning("Not enough keys to allow remove functionality");
+            return;
+        }
+        
+        keys.RemoveAt(index);
+    }
+    
+    public int UpdateKeyTime(int index, float time)
+    {
+        //Remove key at index and place it again in the list to keep it sorted by time
+
+        if (keys == null || index >= keys.Count)
+        {
+            Debug.LogError("Either keys is null or the index does not exist in the list");
+            return -1;
+        }
+
+        Color oldColor = keys[index].Colour;
+        RemoveKey(index);
+
+        return AddKey(oldColor, time);
+    }
+
+    public void UpdateKeyColour(int index, Color colour)
+    {
+        if (keys == null || index >= keys.Count)
+        {
+            Debug.LogError("Either keys is null or the index does not exist in the list");
+            return;
+        }
+
+        ColourKey oldKey = keys[index];
+        
+        keys[index] = new ColourKey(colour, oldKey.Time);
+    }
+
 
     public int NumberOfKeys()
     {
